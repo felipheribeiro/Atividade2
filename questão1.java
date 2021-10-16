4b1bbef60b72ff45bbe6cc8a90949840972bdc45
felipheribeiro <89546698+felipheribeiro@users.noreply.github.com>
2021-10-15 22:16:07 -0300

    Create questão1.java

---
+++ questão1.java
@@ -0,0 +1,33 @@
+public class Paciente {
+
+    public static void main(String[] args) {
+
+        Pacient nmr1 = new Paciente(1, "Luiz Feliphe Silva Ribeiro", "01/06/2004", "Masculino", "Não sabe informar", "Não", "O+");
+
+        System.out.println("\nCodigo: "+nmr1.codigo);
+        System.out.println("Nome: "+nmr1.nome);
+        System.out.println("Data de Nascimento: "+nmr1.dataNascimento);
+        System.out.println("Sexo: "+nmr1.sexo);
+        System.out.println("Plano de Saúde: "+nmr1.planoSaude);
+        System.out.println("Alergia: "+nmr1.alergia);
+        System.out.println("Tipo Sanguíneo: "+nmr1.tipoSanguineo + "\n");
+
+        nmr1.codigo = (nmr1.codigo;)
+        nmr1.nome = ("Wellington De Oliveira Ribeiro";)
+        nmr1.dataNascimento = ("05/02/1976";)
+        nmr1.sexo = ("Masculino";)
+        nmr1.alergia = ("Não";)
+        nmr1.planoSaude = ("Não sabe informar";)
+        nmr1.tipoSanguineo = ("O+";)
+
+        System.out.println("\nCodigo: "+nmr1.codigo);
+        System.out.println("Nome:"+nmr1.nome);
+        System.out.println("Data de Nascimento:"+nmr1.dataNascimento);
+        System.out.println("Sexo: "+nmr1.sexo);
+        System.out.println("Plano de Saúde: "+nmr1.planoSaude);
+        System.out.println("Alergia: "+nmr1.alergia);
+        System.out.println("Tipo Sanguíneo: "+nmr1.tipoSanguineo + "\n");
+
+
+    }
+}
